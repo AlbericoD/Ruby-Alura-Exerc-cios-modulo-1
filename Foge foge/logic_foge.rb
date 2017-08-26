d042e727cce48b12783022cb90129ccbe311106e
AlbericoD <a.diasbarreto@icloud.com>
2017-08-26 20:37:47 -0300

    Adicionado movimentação inimigos
    mesma lógica para movimentar jogador,
    e validação de colisão

--- Foge foge/logic_foge.rb
+++ Foge foge/logic_foge.rb
@@ -2,7 +2,7 @@ require_relative 'ui_foge'
 require_relative 'files_maps'
 
 def joga nome_jogador
-    mapa = arquivo_mapa_leitura 1
+    mapa = arquivo_mapa_leitura 2
     while true  
         desenha mapa
         direcao = movimento_jogador
@@ -12,7 +12,8 @@ def joga nome_jogador
             next
         end
         mapa[heroi[0]] [heroi[1]] = " "
-        mapa[nova_posicao[0]] [nova_posicao[1]] = "H"
+        mapa[nova_posicao[0]] [nova_posicao[1]] = "H" 
+        move_inimigos mapa
     end
 end
 
@@ -30,6 +31,26 @@ def localizar_jogador mapa
         end         
     end   
    #não localizado
+end
+
+def anda_inimigo mapa, linha, coluna
+    posicao = [linha, coluna +1]
+    if posicao_valida_jogador? mapa, posicao
+        mapa[linha][coluna] = " "
+        mapa[posicao[0]][posicao[1]] = "F"
+    end    
+end
+
+def move_inimigos mapa
+    letra_inimigo = "F"
+    mapa.each_with_index do |linha_atual, linha|
+        linha_atual.chars.each_with_index do |letra_inimigo_atual, coluna|
+            inimigo = letra_inimigo_atual == letra_inimigo 
+            if  inimigo
+                anda_inimigo mapa, linha, coluna
+            end
+        end    
+    end    
 end
 
 def nova_posicao_jogador heroi, direcao   
@@ -52,10 +73,10 @@ def posicao_valida_jogador? mapa, posicao
 
     limite_linha = posicao[0] < 0 || posicao[0] >= linhas
     limite_coluna = posicao[1] < 0 || posicao[1] >= colunas
-
+    valor_posicao = mapa[posicao[0]][posicao[1]]
     if limite_linha || limite_coluna  
         return false
-    elsif mapa[posicao[0]][posicao[1]] == "X"    
+    elsif  valor_posicao == "X" || valor_posicao == "F"
         return false 
     else
     true

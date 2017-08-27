5a65cc141bfe4fa8cbad67a4a194d8a888c68873
AlbericoD <a.diasbarreto@icloud.com>
2017-08-27 02:34:30 -0300

    Adcionado movimento dos inimigos
    implementação com ajuda do guilherme

--- Foge foge/logic_foge.rb
+++ Foge foge/logic_foge.rb
@@ -13,7 +13,7 @@ def joga nome_jogador
         end
         mapa[heroi[0]] [heroi[1]] = " "
         mapa[nova_posicao[0]] [nova_posicao[1]] = "H" 
-        move_inimigos mapa
+        mapa = move_inimigos mapa
     end
 end
 
@@ -33,24 +33,41 @@ def localizar_jogador mapa
    #não localizado
 end
 
-def anda_inimigo mapa, linha, coluna
-    posicao = [linha, coluna +1]
-    if posicao_valida_jogador? mapa, posicao
+def posicao_valida_inimigo mapa, novo_mapa, posicao
+    posicoes =[]
+    movimentos = [[+1,0],[0,+1],[0,-1],[-1,0]]
+    movimentos.each do |movimento|
+        nova_posicao = [posicao[0] + movimento[0], posicao[1] + movimento[1]]
+        if posicao_valida_jogador?(mapa, nova_posicao) && posicao_valida_jogador?(novo_mapa, nova_posicao)
+            posicoes << nova_posicao
+        end
+    end
+    posicoes    
+end
+
+def anda_inimigo mapa, novo_mapa, linha, coluna
+    posicoes = posicao_valida_inimigo mapa, novo_mapa, [linha, coluna]
+    if posicoes.empty?
+        return
+    end 
+        aleatoria = rand posicoes.size
+        posicao = posicoes[aleatoria]   
         mapa[linha][coluna] = " "
-        mapa[posicao[0]][posicao[1]] = "F"
-    end    
+        novo_mapa[posicao[0]][posicao[1]] = "F"
 end
 
 def move_inimigos mapa
     letra_inimigo = "F"
+    novo_mapa = copiar_mapa mapa 
     mapa.each_with_index do |linha_atual, linha|
         linha_atual.chars.each_with_index do |letra_inimigo_atual, coluna|
             inimigo = letra_inimigo_atual == letra_inimigo 
             if  inimigo
-                anda_inimigo mapa, linha, coluna
+                anda_inimigo mapa, novo_mapa, linha, coluna
             end
         end    
-    end    
+    end  
+    novo_mapa  
 end
 
 def nova_posicao_jogador heroi, direcao   
@@ -81,4 +98,8 @@ def posicao_valida_jogador? mapa, posicao
     else
     true
     end
+end
+
+def copiar_mapa mapa
+    novo_mapa = mapa.join("\n").tr("F", " ").split ("\n")
 end
